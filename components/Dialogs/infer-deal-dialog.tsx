fd926a2d7c1a050af12994190d4f4cf3a4d34c2b
Chris Nee <cnee2@illinois.edu>
2025-04-22 14:12:41 -0500

    added DialogTitle

--- components/Dialogs/infer-deal-dialog.tsx
+++ components/Dialogs/infer-deal-dialog.tsx
@@ -1,10 +1,7 @@
-"use client";
-
-import * as React from "react";
-import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import { Dialog, DialogContent, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
 import { Bot } from "lucide-react";
-import { InferNewDealComponent } from "../../app/(protected)/infer/InferDealComponent"; // adjust path if needed
+import { InferNewDealComponent } from "../../app/(protected)/infer/InferDealComponent";
 
 export const InferDealDialog = () => {
   return (
@@ -15,6 +12,7 @@ export const InferDealDialog = () => {
         </Button>
       </DialogTrigger>
       <DialogContent className="max-w-5xl">
+        <DialogTitle>Infer Deal</DialogTitle>
         <InferNewDealComponent />
       </DialogContent>
     </Dialog>
