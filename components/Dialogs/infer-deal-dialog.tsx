27b523db9da3bd60b66c65f5cdbda16a7404358c
Chris Nee <cnee2@illinois.edu>
2025-04-24 14:54:29 -0500

    added height limits and moved second collumn to the bottom

--- components/Dialogs/infer-deal-dialog.tsx
+++ components/Dialogs/infer-deal-dialog.tsx
@@ -11,9 +11,11 @@ export const InferDealDialog = () => {
           <Bot className="mr-2 h-4 w-4" /> Infer Deal
         </Button>
       </DialogTrigger>
-      <DialogContent className="max-w-5xl">
+      <DialogContent className="max-w-5xl h-[90vh] max-h-[800px] overflow-hidden flex flex-col">
         <DialogTitle>Infer Deal</DialogTitle>
-        <InferNewDealComponent />
+        <div className="flex-1 overflow-y-auto pr-2">
+          <InferNewDealComponent />
+        </div>
       </DialogContent>
     </Dialog>
   );
